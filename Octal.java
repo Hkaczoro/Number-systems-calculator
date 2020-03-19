1a131d6df2c9f7ed0e7f41f4975dcb9b391b53f2
Hubert Kaczorowski <h.kaczorowski@stud.elka.pw.edu.pl>
2020-03-19 18:49:16 +0100

    Dodanie pustej klasy Octal

---
+++ Octal.java
@@ -0,0 +1,8 @@
+/**
+ * Klasa zawiera metody na zmiane z systemu ósemkowego na inne(binarny, dziesietny, szesnastkowy)
+ */
+public class Octal {
+    public void binarny(int liczba){
+        
+    }
+}
